925fe015e8bf35a610d6d90a78872644077e335b
wugang <wugang200820@163.com>
2013-09-10 15:30:45 +0800

    deploy detup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,57 +1,75 @@
-# -*- encoding : utf-8 -*-
-set :stages, %w(production staging qa)
-set :default_stage, ENV['env'] || "production"
-require 'capistrano/ext/multistage'
-
-set :application, ""
-set :repository,  ""
-
-set :scm, :git 
-
-ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/public_key.pem"]
-set :use_sudo, false
-set :user, "ubuntu"
-set :deploy_to, ""
-
-
-set :rvm_ruby_string, "1.9.3@global"
-set :rvm_autolibs_flag, "read-only"        # more info: rvm help autolibs
-before 'deploy:setup', 'rvm:install_rvm'   # install RVM
-before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, OR:
-before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :application, "cardsys"
+set :repository,  "git@github.com:cmingxu/cardsys.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "112.124.57.67"                          # Your HTTP server, Apache/etc
+role :app, "112.124.57.67"                          # This may be the same as your `Web` server
+role :db,  "112.124.57.67", :primary => true # This is where Rails migrations will run
+role :db,  "112.124.57.67"
+
+set :user, "deploy"
+set :password, "Xuchunming123"
+
+set :use_sudo, false
+
+set :deploy_to, "/home/deploy/code/#{application}"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+#
+default_run_options[:pty] = true
+set :rvm_ruby_string,  "ruby-1.9.3-p392"
+#set :rvm_type, :user
+set :rvm_bin_path, "/home/deploy/.rvm/bin"
+
+#after 'deploy:setup', 'rvm:install_rvm'
+#after 'deploy:setup', 'rvm:install_ruby'
 
 
 namespace :deploy do
-  task :build_gems, :roles => :app do
-    desc "Building gems"
+  desc "cause Passenger to initiate a restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+
+  task :start do
+    sudo "/opt/nginx/sbin/nginx"
+  end
+
+  task :stop do
+    sudo "/opt/nginx/sbin/nginx -s stop"
+  end
+
+  task :update_bundle do
     run "cd #{release_path} && bundle install"
   end
 
-  task :create_database do
-    desc "Migrating database"
-    run "cd #{release_path} && rake db:create RAILS_ENV=#{stage}"
-  end
-
-  task :migrations do
-    desc "Migrating database"
-    run "cd #{release_path} && rake db:migrate RAILS_ENV=#{stage}"
-  end
-
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-
-  after "deploy:update_code","deploy:build_gems", "db:copy_configuration", "deploy:migrations", "deploy:cleanup"
 end
 
 namespace :db do
-  task :copy_configuration do
-    run "cp #{release_path}/config/database.yml.default #{release_path}/config/database.yml"
+  task :db_config, :except => { :no_release => true }, :role => :app do
+    run "cp -f #{release_path}/config/database.template #{release_path}/config/database.yml"
   end
 end
 
+after "deploy:update_code", "db:db_config", "deploy:update_bundle", "deploy:migrate"
 
-load 'deploy/assets'
+
+before 'deploy:setup', 'rvm:install_rvm'
+before 'deploy:setup', 'rvm:install_ruby'
+
