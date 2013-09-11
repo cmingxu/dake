2f7e210bfdbcbf488198cfe391f0ba9faf37ac4e
Kevin.Xu <cming.xu@gmail.com>
2013-09-11 12:48:43 +0800

    change

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,16 +3,20 @@ class WelcomeController < ApplicationController
   layout "public"
 
   def index
+     redirect_to admin_dashboard_path
   end
 
   def dashboard
+     redirect_to admin_dashboard_path
   end
 
   def about
     @page = Page.find_by_slug "about"
+     redirect_to admin_dashboard_path
   end
 
   def team
+     redirect_to admin_dashboard_path
   end
 
   def sign_as
