dabd05f045024708509fd1e19939dfd22f707d3f
Kevin.Xu <cming.xu@gmail.com>
2013-10-02 20:05:29 +0800

    cheliang guanli

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,8 @@
 # -*- encoding : utf-8 -*-
 class Vehicle < ActiveRecord::Base
+  LEIXNG = %w(大客 小客 大货 小货 翻斗)
   attr_accessible :leixing, :paizhao, :xinghao
+
+  validates :leixing, :presence =>  { :message => "类型需填写" }
+  validates :xinghao, :presence => { :message => "型号需要填写" }
 end
